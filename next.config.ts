ee57e4208e1d48db5859152aa03e2cccc963a5ff
Akhie Najhan Atifa <akhie12340@gmail.com>
2026-06-17 19:11:51 +0700

    config: allow dev origin 192.168.0.102 for tablet HMR

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["192.168.0.102", "192.168.0.102:3000"],
 };
 
 export default nextConfig;
